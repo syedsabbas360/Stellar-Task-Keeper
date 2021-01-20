bd2efada042f2deddd9bee9434e20919ba69d497
Stellar Digital Design <syedsabbas360@gmail.com>
2021-01-19 18:21:26 -0600

    ui and functionality work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
+  // filter button values
+  const [filter, setFilter] = useState('All');
+  
+  const FILTER_MAP = {
+    All: () => true,
+    Active: task => !task.completed,
+    Completed: task => task.completed
+  };
+
+  //collect an array of FILTER_NAMES
+  const FILTER_NAMES = Object.keys(FILTER_MAP);  
+  
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map(task => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+        saveTask={saveTask}
+      />
+    ));
+
+  const filterList = FILTER_NAMES.map( name => {
+    return(
+      <FilterButton
+        key={name} 
+        name={name} 
+        isPressed={name === filter}
+        setFilter={setFilter}
+      />
+    )
+    
+  })
+
+  function addTask(name){
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+
+    /* setTasks takes the newTask that's created 
+    and adds it to the end of tasks array of objects */
+    setTasks([...tasks, newTask]); 
+
+  };
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function saveTask(id, str) {
+    const editExistingTaskVal = tasks.map(task => {
+      if(id===task.id) {
+        task.name = str
+        return task
+      }
+      return task
+    });
+    setTasks(editExistingTaskVal); 
+
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map( task => {
+      if( id === task.id){
+        return { ...task, newName }
+      }
+      return task;
+    })
+    setTasks(editedTaskList);
+  }
+
+  function toggleTaskCompleted(id){
+    const updatedTasks = tasks.map(task => {
+      if( id === task.id){
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks)
+  }  
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>Stellar Task Keeper</h1>
+        <Form addTask={addTask}/>
+      <div className="filters btn-group stack-exception">
+        { filterList }
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+        {taskList}
     </div>
   );
 }
